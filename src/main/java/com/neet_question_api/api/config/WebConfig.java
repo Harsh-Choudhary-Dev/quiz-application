455c7792d72f55a94e93e7d3c9d73fe576865efa
Harsh <harshchoudhary50226@gmail.com>
2024-12-20 16:49:52 +0530

    congiq

--- src/main/java/com/neet_question_api/api/config/WebConfig.java
+++ src/main/java/com/neet_question_api/api/config/WebConfig.java
@@ -6,13 +6,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Allow CORS for all endpoints
-                .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500","http://127.0.0.1:5501", "http://localhost:5501","http://127.0.0.1:5502", "http://localhost:5502") // Allow requests from these origins
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow these methods
-                .allowedHeaders("*") // Allow all headers
-                .allowCredentials(true); // Allow credentials (e.g., cookies, headers like x-api-key)
+        registry.addMapping("/**")
+                .allowedOrigins("*") // Replace with specific domains as needed
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization")
+                .allowCredentials(false)
+                .maxAge(3600);
     }
 }
